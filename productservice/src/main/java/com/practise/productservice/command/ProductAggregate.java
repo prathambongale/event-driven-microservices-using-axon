7c8e1b644c171d179f833b03fbdeaab1ff25b2d5
prathambongale <prathambongale@hotmail.com>
2023-01-10 05:50:19 +0200

    progress till now i.e 10-01-2023 5:50am

---
+++ productservice/src/main/java/com/practise/productservice/command/ProductAggregate.java
@@ -0,0 +1,58 @@
+package com.practise.productservice.command;
+
+import java.math.BigDecimal;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+import com.practise.productservice.core.event.ProductCreatedEvent;
+
+import lombok.NoArgsConstructor;
+
+@Aggregate
+@NoArgsConstructor
+public class ProductAggregate {
+    
+    @AggregateIdentifier
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+    @CommandHandler
+    public ProductAggregate(CreateProductCommand createProductCommand) {
+        // Validate Create Product Command
+
+        if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Price cannot be less or equal than zero");
+        }
+
+        if (createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+
+        AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    /**
+     * Will be used to initilaize currect state of aggregate 
+     * avoid adding any business logic 
+     * and use it only to update the aggregate state
+    */  
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.title = productCreatedEvent.getTitle();
+        this.quantity = productCreatedEvent.getQuantity();
+    }
+
+}
--- productservice/src/main/java/com/practise/productservice/rest/ProductController.java
+++ productservice/src/main/java/com/practise/productservice/rest/ProductController.java
@@ -1,13 +1,18 @@
 package com.practise.productservice.rest;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import com.practise.productservice.command.CreateProductCommand;
 
 /**
  * ProductController
@@ -16,13 +21,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
     @Autowired
-    private Environment env;
+    public ProductController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
 
     @PostMapping
-    public String createProduct() {
-        return "HTTP POST Handled";
-    }    
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+        CreateProductCommand createProductCommand = CreateProductCommand
+                .builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        String returnValue;
+        
+        try {
+            // send and wait command will send and wait the command to exicute
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception exception) {
+            returnValue = exception.getLocalizedMessage();
+        }
+        
+        return returnValue;
+    }
 
     @GetMapping
     public String getProduct() {
